1f10bd01d47a7c39b21f4b198b73ebbf449bab3f
TonmoyGoon <tonmoygoon@gmail.com>
2022-06-27 16:21:26 +0600

    mealitem updated

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,8 @@
 import classes from "./AvailableMeals.module.css";
 
+import MealItem from "./MealItem/MealItem";
+import Card from "../UI/Card";
+
 const DUMMY_MEALS = [
     {
         id: 'm1',
@@ -28,10 +31,21 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => meal.name);
-    return <section className={classes.meals}>
-        <ul>{mealsList}</ul>
-    </section>
+    const mealsList = DUMMY_MEALS.map((meal) => (
+        <MealItem
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />
+    ));
+    return (
+        <section className={classes.meals}>
+            <Card>
+                <ul>{mealsList}</ul>
+            </Card>
+        </section>
+    )
 }
 
 export default AvailableMeals;
