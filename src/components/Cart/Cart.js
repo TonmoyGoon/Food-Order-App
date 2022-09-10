eac08e7af574781bac7106481cd9c967ec482df8
TonmoyGoon <tonmoygoon@gmail.com>
2022-09-10 21:12:38 +0600

    functioning cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,14 +6,14 @@ const Cart = props => {
             [{ id: "c1", name: "Sushi", amount: 3, price: 22.99 }].map((item) => <li>{item.name}</li>)
         }
     </ul>
-    return <Modal>
+    return <Modal onClose={props.onClose}>
         {cartItems}
         <div className={classes.total}>
             <span>Total amount</span>
             <span>45.87</span>
         </div>
         <div className={classes.actions}>
-            <button className={classes["button-alt"]}>Close</button>
+            <button className={classes["button-alt"]} onClick={props.onClose}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
     </Modal>
